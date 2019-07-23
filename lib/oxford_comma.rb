393000a131e965d71755af3f11df0153166cf1c4
Stef Spears <spears.sj87@gmail.com>
2019-07-23 04:07:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+  if array.length == 2 
+    array.join(" and ")
+  elsif array.length == 1 
+    return array.join
+    binding.pry
+  else last_item = array.pop()
+    array << "and #{last_item}"
+    array.join(", ")
+  end
 end
