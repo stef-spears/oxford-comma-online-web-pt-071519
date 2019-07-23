c2f56132e8d76f68c0ed39506e0156e02c69bb9f
Stef Spears <spears.sj87@gmail.com>
2019-07-23 03:59:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,10 @@
 def oxford_comma(array)
   if array.length == 2 
     array.join(" and ")
-  else last_item = " and + #{array[-1]}"
+  else last_item = array.pop()
+    last_item
+    
+    " and + #{array[-1]}"
     array.pop()
     array.join(", ")
 end
