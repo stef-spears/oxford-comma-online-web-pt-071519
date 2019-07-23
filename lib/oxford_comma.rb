20523ff967b418d796e82cbb7ab1041c9bf6712a
Stef Spears <spears.sj87@gmail.com>
2019-07-23 04:01:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,13 @@
+require "pry"
+
 def oxford_comma(array)
   if array.length == 2 
     array.join(" and ")
+    
+    binding.pry
+    
   else last_item = array.pop()
     array << " and + #{last_item}"
     array.join(", ")
+  end
 end
